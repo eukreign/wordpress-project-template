6074418d3f94814bb7b23ecb8d6f1e5ef4094cd6
Lex Berezhny <lex@berezhny.net>
2013-06-11 14:17:12 -0400

    updated project fabfile

--- project-fabfile.py
+++ project-fabfile.py
@@ -1,15 +1,16 @@
-from os import getcwd, sep
-from fabric.api import cd, run, env
+import os
+from fabric.api import cd, run, local, env, get
 from fabric.contrib.project import rsync_project
 
 env.hosts = ['grafton2@graftonweb.com']
 
-project = getcwd().split(sep)[-1]
+project = os.getcwd().split(os.sep)[-1]
+path = os.path.join('www', project)
 
 def rsync():
     rsync_project(
         remote_dir='www',
-        exclude=['wordpress',
+        exclude=['wordpress', 'fabfile.py',
                  'wp-config.php', 'local.wp-config.php',
                  '.hg', '.idea', 'error_log', '.hgignore'],
         extra_opts='--chmod=go-w'
@@ -17,5 +18,12 @@ def rsync():
 
 def push():
     rsync()
-    with cd('www/'+project):
+    with cd(path):
         run('cp production.wp-config.php wp-config.php')
+
+def db():
+    dbfile = 'database.sql'
+    with cd(path):
+        run('wp db export '+dbfile)
+    get(os.path.join(path, dbfile), dbfile)
+    local('wp db import '+dbfile)
